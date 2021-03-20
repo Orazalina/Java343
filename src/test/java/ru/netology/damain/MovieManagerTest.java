4fa0d104a22f384f2357bc18c0f836cd931fffaa
Anara Orazalina <orazalina.anara@inbox.ru>
2021-03-20 19:22:34 +0600

    test added

---
+++ src/test/java/ru/netology/damain/MovieManagerTest.java
@@ -0,0 +1,66 @@
+package ru.netology.damain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieManagerTest {
+    private MovieManager manager = new MovieManager();
+    MovieItem first = new MovieItem(1, "Постер", "Бладшот", "Боевик");
+    MovieItem second = new MovieItem(2, "Постер", "Вперёд", "Мультфильм");
+    MovieItem third = new MovieItem(3, "Постер", "Отель Белград", "Комедия");
+    MovieItem fourth = new MovieItem(4, "Постер", "Джентльмены", "Боевик");
+    MovieItem fifth = new MovieItem(5, "Постер", "Человек-невидимка", "Ужасы");
+
+    public void setUp() {
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+    }
+
+    @Test
+    void shouldGetEmpty() {
+        MovieItem[] expected = new MovieItem[0];
+        MovieItem[] actual = manager.getAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldAddToEmpty() {
+        manager.add(first);
+        MovieItem[] expected = new MovieItem[]{first};
+        MovieItem[] actual = manager.getAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldAddToExisted() {
+        manager.add(first);
+        MovieItem[] expected = new MovieItem[]{first};
+        MovieItem[] actual = manager.getAll();
+        assertArrayEquals(expected, actual);
+        manager.add(second);
+        MovieItem[] expectedAdded = new MovieItem[]{second, first};
+        MovieItem[] actualAdded = manager.getAll();
+        assertArrayEquals(expectedAdded, actualAdded);
+    }
+
+    @Test
+    void shouldGetMoviesForFeed() {
+        setUp();
+        MovieItem[] expected = new MovieItem[]{fifth, fourth, third, second, first};
+        MovieItem[] actual = manager.getMoviesForFeed();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldGetMoviesForFlexFeed() {
+        manager = new MovieManager(3);
+        setUp();
+        MovieItem[] expected = new MovieItem[]{fifth, fourth, third};
+        MovieItem[] actual = manager.getMoviesForFeed();
+        assertArrayEquals(expected, actual);
+    }
+}
